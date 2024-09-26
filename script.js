8760c55b5fa049b0a45c9084f03235cc47fe6df1
Vidhyaeswarasamy <vidhyaeswarasamy@gmail.com>
2024-09-27 00:05:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,58 +19,84 @@ addpopupbutton.addEventListener("click",function(){
 })
 
 
-var cancelpopupbutton=document.getElementById("cancel-popup")
-cancelpopupbutton.addEventListener("click",function(event){
+var cancelpopup=document.getElementById("cancel-popup")
+cancelpopup.addEventListener("click",function(event){
     event.preventDefault()
     popupoverlay.style.display="none"
     popupbox.style.display="none"
 })
-var container=document.querySelector(".container");
-var addbook=document.getElementById("add-book");
-var booktitleinput=document.getElementById("book-title-input");
-var bookauthorinput=document.getElementById("book-author-input");
-var shortdescriptioninput=document.getElementById("short-description-input");
-var linkpageinput=document.getElementById("link-page");
+var container = document.querySelector(".container");
+var addbook = document.getElementById("add-book");
+var booktitleinput = document.getElementById("book-title-input");
+var bookauthorinput = document.getElementById("book-author-input");
+var shortdescriptioninput = document.getElementById("short-description-input");
 
+// Load existing books from local storage
+window.onload = function() {
+    var books = JSON.parse(localStorage.getItem("books")) || [];
+    books.forEach(function(book) {
+        displayBook(book);
+    });
+};
 
-addbook.addEventListener("click",function(event){
-    event.preventDefault()
-    var div=document.createElement("div")
-    div.setAttribute("class","book-container")
-    div.innerHTML=`<h2>${booktitleinput.value}</h2>
-    <h5>${bookauthorinput.value}</h5>
-    <p>${shortdescriptioninput.value}</p>
-    <button onclick="linkbook()">${linkpageinput.value} Read Book</button>
-    <button onclick="deletebook(event)">Delete</button>`
+// Add book event listener
+addbook.addEventListener("click", function(event) {
+    event.preventDefault();
     
-    container.append(div)
-    popupoverlay.style.display="none"
-    popupbox.style.display="none";
+    var book = {
+        title: booktitleinput.value,
+        author: bookauthorinput.value,
+        description: shortdescriptioninput.value
+    };
+    
+    // Store in local storage
+    var books = JSON.parse(localStorage.getItem("books")) || [];
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+    
+    // Display the book
+    displayBook(book);
+
+    // Clear inputs
+    booktitleinput.value = '';
+    bookauthorinput.value = '';
+    shortdescriptioninput.value = '';
+
+    // Close popup
+    popupoverlay.style.display = "none";
+    popupbox.style.display = "none";
+});
+
+// Function to display a book
+function displayBook(book) {
+    var div = document.createElement("div");
+    div.setAttribute("class", "book-container");
+    div.innerHTML = `
+        <h2>${book.title}</h2>
+        <h3>${book.author}</h3>
+        <p>${book.description}</p>
+        <button onclick="deletebook(event)">Delete</button>
+    `;
+    container.append(div);
+}
+
+// Delete book function
+function deletebook(event) {
+    var bookContainer = event.target.parentNode;
+    bookContainer.remove();
+
+    // Update local storage
+    var books = JSON.parse(localStorage.getItem("books")) || [];
+    books = books.filter(book => {
+        return book.title !== bookContainer.querySelector('h1').innerText;
+    });
+    localStorage.setItem("books", JSON.stringify(books));
+}
 
 
-    
-localStorage.setItem('booktitle',booktitleinput);
-localStorage.setItem('bookauthor',bookauthorinput);
-localStorage.setItem('shortdescription',shortdescriptioninput);
-localStorage.setItem('linkbook',linkpageinput);
-    
-})
-    
-function deletebook(event){
-    event.target.parentElement.remove()
-}
-function gotolink(link){
-  console.log(link.value);
-  window.open(link.value);
-}
-
-let popup=document.getElementById("popup");
-function openpopup(){
-    popup.classList.add("open-popup");
-}
-function closepopup(){
-    popup.classList.remove("open-popup");
-}
+  function deletebook(event){
+  event.target.parentElement.remove()
+  }
 
 
 
